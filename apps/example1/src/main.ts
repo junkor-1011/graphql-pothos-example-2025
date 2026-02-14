5c2172797d9f9785aa726123dcb4b68f85d15cfa
junkor-1011 <tomoyukizxy@gmail.com>
2026-02-14 09:50:15 +0900

    fix

--- apps/example1/src/main.ts
+++ apps/example1/src/main.ts
@@ -1,9 +1,10 @@
 // biome-ignore assist/source/organizeImports: MUST be called first
 import { startOtel } from "./otel";
 startOtel();
+
 import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
-import { builder } from "./buider";
+import { builder } from "./builder";
 import { logger } from "./logger";
 
 const isProd: boolean = process.env.NODE_ENV === "production";
